3b84ebcc29d48287c900b42949a7108030a596da
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-03-10 23:52:08 +0100

    intégrer les boutons de filtrage

--- scripts/script.js
+++ scripts/script.js
@@ -2,74 +2,92 @@
 async function buildButtons(){
     const filter_btns = document.querySelector('.filter_btns'); // sélectionner le bloc qui comporte les boutons
 
-
     //créer le bouton de tri d'ingrédients
-    const group_ingredients_button = document.createElement('div');
-    group_ingredients_button.classList.add('btn_group');
-    group_ingredients_button.setAttribute('role','group');
+    const group_button = document.createElement('div');
+    group_button.classList.add('btn_group');
+    group_button.setAttribute('role','group');
 
+    // const text = document.createElement('div');
+    // text.classList.add('text');
+    // text.textContent='bonjour';
+    // text.style.backgroundColor='blue';
+    // ingredients_button.appendChild(text);
+
+    // const ingredients_menu = document.createElement('div');
+    // ingredients_menu.classList.add('ingredients_menu');
+    // ingredients_menu.style.display='none';
+    // ingredients_menu.setAttribute('aria-labelledby','ingredients_menu_group');
+    // const ingredient = document.createElement('a');
+    // ingredient.classList.add('ingredient');
+    // ingredient.setAttribute('href','#');
+
+    //créer le bouton de tri d'ingredients
     const ingredients_button = document.createElement('button');
     ingredients_button.classList.add('filter_button');
-    ingredients_button.classList.add('ingredients_button');
-    ingredients_button.setAttribute('title','Ingrédients');
     ingredients_button.setAttribute('type','button');
-    ingredients_button.setAttribute('data-bs-toggle','dropdown');
-    ingredients_button.setAttribute('aria-haspopup','true');
-    ingredients_button.setAttribute('aria-expanded','false');
-    ingredients_button.textContent = 'Ingrédients';
     ingredients_button.style.backgroundColor = '#3282f7';
 
-    const ingredients_menu = document.createElement('div');
-    ingredients_menu.classList.add('ingredients_menu');
+    const text_button_ingredients = document.createElement('p');
+    text_button_ingredients.classList.add('text_btn');
+    text_button_ingredients.textContent='Ingrédients';
+    ingredients_button.appendChild(text_button_ingredients);
 
-    ingredients_menu.setAttribute('aria-labelledby','ingredients_menu_group');
-    const ingredient = document.createElement('a');
-    ingredient.classList.add('ingredient');
-    ingredient.setAttribute('href','#');
-
-    
-    ingredients_menu.appendChild(ingredients_button);
-    ingredients_menu.appendChild(ingredient);
-    group_ingredients_button.appendChild(ingredients_menu);
-    group_ingredients_button.appendChild(ingredients_button);
+    const icon_button_ingredients = document.createElement('i');
+    icon_button_ingredients.classList.add('fa-solid');
+    icon_button_ingredients.classList.add('fa-caret-down');
+    ingredients_button.appendChild(icon_button_ingredients);
 
 
     //créer le bouton de tri d'Appareils
-    const group_appareils_button = document.createElement('div');
-    group_appareils_button.classList.add('btn_group');
-    group_appareils_button.setAttribute('role','group');
+    // const group_appareils_button = document.createElement('div');
+    // group_appareils_button.classList.add('btn_group');
+    // group_appareils_button.setAttribute('role','group');
 
     const appareils_button = document.createElement('button');
     appareils_button.classList.add('filter_button');
-    appareils_button.classList.add('appareils_button');
-    appareils_button.setAttribute('title','Appareils');
     appareils_button.setAttribute('type','button');
-    appareils_button.setAttribute('data-bs-toggle','dropdown');
-    appareils_button.textContent = 'Appareils';
     appareils_button.style.backgroundColor = '#68d9a4';
-    group_appareils_button.appendChild(appareils_button);
+
+    const text_button_appareils = document.createElement('p');
+    text_button_appareils.classList.add('text_btn');
+    text_button_appareils.textContent='Appareils';
+    appareils_button.appendChild(text_button_appareils);
+
+    const icon_button_appareils = document.createElement('i');
+    icon_button_appareils.classList.add('fa-solid');
+    icon_button_appareils.classList.add('fa-caret-down');
+    appareils_button.appendChild(icon_button_appareils);
 
 
-    //créer le bouton de tri d'Appareils
-    const group_ustensiles_button = document.createElement('div');
-    group_ustensiles_button.classList.add('btn_group');
-    group_ustensiles_button.setAttribute('role','group');
 
+    //créer le bouton de tri d'Ustensiles
     const ustensiles_button = document.createElement('button');
     ustensiles_button.classList.add('filter_button');
-    ustensiles_button.classList.add('ustensiles_button');
-    ustensiles_button.setAttribute('title','Ustensiles');
     appareils_button.setAttribute('type','button');
-    appareils_button.setAttribute('data-bs-toggle','dropdown');
-    ustensiles_button.textContent = 'Ustensiles';
     ustensiles_button.style.backgroundColor = '#ed6454';
-    group_ustensiles_button.appendChild(ustensiles_button);
 
+    const text_button_ustensiles = document.createElement('p');
+    text_button_ustensiles.classList.add('text_btn');
+    text_button_ustensiles.textContent='Appareils';
+    ustensiles_button.appendChild(text_button_ustensiles);
+
+    const icon_button_ustensiles = document.createElement('i');
+    icon_button_ustensiles.classList.add('fa-solid');
+    icon_button_ustensiles.classList.add('fa-caret-down');
+    ustensiles_button.appendChild(icon_button_ustensiles);
+
+
+    group_button.appendChild(ingredients_button);
+    group_button.appendChild(appareils_button);
+    group_button.appendChild(ustensiles_button);
+    group_button.appendChild(ustensiles_button);
+
+
+    filter_btns.appendChild(group_button);
 
     //ajouter les boutons de filtre dans le même élément
-    filter_btns.appendChild(group_ingredients_button);
-    filter_btns.appendChild(group_appareils_button);
-    filter_btns.appendChild(group_ustensiles_button);
+    filter_btns.appendChild(group_button);
+    filter_btns.appendChild(group_button);
 
 }
 buildButtons();

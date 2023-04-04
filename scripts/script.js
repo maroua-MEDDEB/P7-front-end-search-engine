f5bfad15ea40d466acf1cb6984d54e891547c96a
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-04-04 18:45:37 +0200

    structurer les boutons llors de l'affichage de chaque liste

--- scripts/script.js
+++ scripts/script.js
@@ -6,52 +6,43 @@
 const filter_btns = document.querySelector('.filter_btns'); // sélectionner le bloc qui comporte les boutons
 let filtred = [];
 
-let ingredients;
 let filtred_ingredients = []; 
 const filter_inreg = () => {
     recipes.forEach((recipe) => {
-        ingredients = recipe.ingredients;
+        const ingredients = recipe.ingredients;
 
         ingredients.forEach((ingr) => {
             if(filtred_ingredients.indexOf(ingr.ingredient.toLowerCase()) === -1) {
-                console.log(filtred_ingredients.push(ingr.ingredient.toLowerCase())
-                );
-                // filtred_ingredients.push(ingr.ingredient.toLowerCase());
+                filtred_ingredients.push(ingr.ingredient.toLowerCase());
             }
         });    
     })
 }
 
-let appliance;
 let filtred_appliance = [];
 const filter_app = () => {
     recipes.forEach((el) => {
-        appliance = el.appliance;
-        
-        filtred_appliance.push(appliance);
-
+        // Si le tableau filtred_appliance ne contient pas  déjà "appliance" donc on ajoute ce dernier
+        if(filtred_appliance.indexOf(el.appliance.toLowerCase()) === -1 ) {
+            filtred_appliance.push(el.appliance.toLowerCase());
+        }
     })
 };
 
-let ustensils;
 let filtred_ustensils = [];
 const filter_ust = () => {
     recipes.forEach((recipe) => {
-        ustensils = recipe.ustensils;
+        const ustensils = recipe.ustensils;
 
         ustensils.forEach((ust) => {
-           
             if(filtred_ustensils.indexOf(ust.toLowerCase()) === -1) {
                 filtred_ustensils.push(ust.toLowerCase());
-                console.log(filtred_ustensils.push(ust.toLowerCase())
-                );
             }
-            console.log(ust.toLowerCase());
         });    
     })
 }
 
-const build_filter_list = (backgroundColor, filtred) => {
+const build_filter_list = (backgroundColor, research_element, filtred) => {
     const filter = document.createElement('div');
     filter.classList.add('filter');
     filter.style.backgroundColor = backgroundColor;
@@ -61,11 +52,12 @@ const build_filter_list = (backgroundColor, filtred) => {
 
     const filter_search_input = document.createElement('input');
     filter_search_input.setAttribute('type', 'text');
-    filter_search_input.setAttribute('placeholder', 'Rechecher un ingrédient');
+    filter_search_input.setAttribute('placeholder', research_element);
 
     const filter_search_icon = document.createElement('i');
     filter_search_icon.classList.add('fa-solid');
     filter_search_icon.classList.add('fa-chevron-up');
+
 
     const filter_list = document.createElement('div');
     filter_list.classList.add('filter_list');
@@ -83,11 +75,38 @@ const build_filter_list = (backgroundColor, filtred) => {
         const filter_list_item = document.createElement('div');
         filter_list_item.classList.add('filter_list_item');
         filter_list_item.textContent = item;
+        filter_list_item.style.cursor = 'pointer';
+        
         filter_list.appendChild(filter_list_item);
+
+        filter_list_item.addEventListener('click', () => {
+            // filter_btns.prepend(build_tags());
+            const build_tags = () => {
+                const tags_container = document.createElement('div');
+                tags_container.classList.add('tags_container');
+
+                const tag = document.createElement('div');
+                tag.classList.add('tag');
+                tag.style.backgroundColor = backgroundColor;
+            
+                const tag_text = document.createElement('span');
+                tag_text.classList.add('tag_text');
+                tag_text.textContent = item;
+            
+                const tag_icon = document.createElement('i');
+                tag_icon.classList.add('fa-regular');
+                tag_icon.classList.add('fa-circle-xmark');
+                      
+                tag.appendChild(tag_text);
+                tag.appendChild(tag_icon);
+                tags_container.appendChild(tag);
+            
+                return tags_container;
+            }
+            filter_btns.prepend(build_tags());
+        })
     });
-
-   
-
+    
     return filter;
 }
 
@@ -168,32 +187,38 @@ const  buildButtons = () => {
 
 buildButtons();
 
-const ingredients_button = document.querySelector('.ingredients_button');
-const appliances_button = document.querySelector('.appliances_button');
-const ustensiles_button = document.querySelector('.ustensiles_button');
-
-
 const ingredients_button_group = document.querySelector('.ingredients_button_group');
+const ingredients_button = document.querySelector('.ingredients_button');
 const appliances_button_group = document.querySelector('.appliances_button_group');
+const appliances_button = document.querySelector('.appliances_button');
 const ustensiles_button_group = document.querySelector('.ustensiles_button_group');
-
-
+const ustensiles_button = document.querySelector('.ustensiles_button');
 
 ingredients_button.addEventListener('click', (event) => {
     ingredients_button.style.display = 'none';
-    ingredients_button_group.appendChild(build_filter_list('#3282f7', filtred_ingredients));
+    ingredients_button_group.appendChild(build_filter_list('#3282f7', 'Rechercher un ingrédient', filtred_ingredients));
+
+    const filter_list = event.currentTarget.parentNode.querySelector('.filter_list');
+    
+    const temp_element = document.createElement('div');
+    temp_element.style.width = filter_list.offsetWidth + 40 +'px';
+
+    ingredients_button_group.appendChild(temp_element);
 });
 
 appliances_button.addEventListener('click', () => {
     appliances_button.style.display = 'none';
-    appliances_button_group.appendChild(build_filter_list('#68d9a4', filtred_appliance));
+    appliances_button_group.appendChild(build_filter_list('#68d9a4', 'Rechercher une appareil', filtred_appliance));
+
+    const filter_list = event.currentTarget.parentNode.querySelector('.filter_list');
+    
+    const temp_element = document.createElement('div');
+    temp_element.style.width = filter_list.offsetWidth + 40 +'px';
+
+    appliances_button_group.appendChild(temp_element);
 });
 
 ustensiles_button.addEventListener('click', () => {
     ustensiles_button.style.display = 'none';
-    ustensiles_button_group.appendChild(build_filter_list('#ed6454', filtred_ustensils));
+    ustensiles_button_group.appendChild(build_filter_list('#ed6454', 'Rechercher un unstentil', filtred_ustensils));
 });
-
-
-
-
